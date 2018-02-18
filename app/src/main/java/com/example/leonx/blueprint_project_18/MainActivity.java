14a3bb6d1bb4611d11f963d5a0bf713c633aa514
LEON-LAPTOP\leonx <lxie21@mlschools.org>
2018-02-18 12:17:17 -0500

    test basic ui

--- app/src/main/java/com/example/leonx/blueprint_project_18/MainActivity.java
+++ app/src/main/java/com/example/leonx/blueprint_project_18/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.leonx.blueprint_project_18;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void displayToast()
+    {
+        Toast myToast = Toast.makeText(getApplicationContext(), "hello again", Toast.LENGTH_LONG);
+        myToast.show();
     }
 }
